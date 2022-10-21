d441175180bfaf32b4e2ce689c98c5a10285b125
VW <34698193+QuietEvolver@users.noreply.github.com>
2022-10-21 09:41:02 -0700

    It will update through value declaration of index intakes on js.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// BI
+
+
+// UI
+
+const questionOne = document.getElementById("question-one").ariaValueMax;
+const questionTwo = document.getElementById("question-Two").ariaValueMax;
+const questionThree = document.getElementById("question-three").ariaValueMax;
+const questionFour = document.getElementById("question-four").ariaValueMax;
+const questionFive = document.getElementById("question-five").ariaValueMax;
