d4ff0a2c53b6ecf32d222773737be0cb57756191
VW <34698193+QuietEvolver@users.noreply.github.com>
2022-10-21 09:42:28 -0700

    It will parse ints of declared index intakes on js.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@
 
 // UI
 
-const questionOne = document.getElementById("question-one").ariaValueMax;
-const questionTwo = document.getElementById("question-Two").ariaValueMax;
-const questionThree = document.getElementById("question-three").ariaValueMax;
-const questionFour = document.getElementById("question-four").ariaValueMax;
-const questionFive = document.getElementById("question-five").ariaValueMax;
+const questionOne = parseInt(document.getElementById("question-one").value);
+const questionTwo = parseInt(document.getElementById("question-Two").value);
+const questionThree = parseInt(document.getElementById("question-three").value);
+const questionFour = parseInt(document.getElementById("question-four").value);
+const questionFive = parseInt(document.getElementById("question-five").value);
