72131c930f54d29f0037be1289c03eac8ace524c
VW <34698193+QuietEvolver@users.noreply.github.com>
2022-10-21 10:24:18 -0700

    It will update html languages, question placeholders on index, and add hidden class; add tolalVal to js.

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@
 */
 
 // UI
-
+let totalValue = null;
 const questionOne = parseInt(document.getElementById("question-one").value);
 const questionTwo = parseInt(document.getElementById("question-Two").value);
 const questionThree = parseInt(document.getElementById("question-three").value);
@@ -18,6 +18,8 @@ const questionFive = parseInt(document.getElementById("question-five").value);
 const questionSix = parseInt(document.getElementById("question-six").value);
 
 document.getElementById("language-1").setAttribute("class", "hidden");
+document.getElementById("language-2").setAttribute("class", "hidden");
+document.getElementById("language-3").setAttribute("class", "hidden");
 
 /* Given langs: a, b, c:
   Q1 - a
