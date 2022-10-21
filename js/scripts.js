aa1436b50551928e0d1affd64a3b381e0bb6dfd7
VW <34698193+QuietEvolver@users.noreply.github.com>
2022-10-21 09:55:04 -0700

    It will add submit button, declare an addl ques6 on index, and js.

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,13 @@ const questionTwo = parseInt(document.getElementById("question-Two").value);
 const questionThree = parseInt(document.getElementById("question-three").value);
 const questionFour = parseInt(document.getElementById("question-four").value);
 const questionFive = parseInt(document.getElementById("question-five").value);
+const questionSix = parseInt(document.getElementById("question-six").value);
+
+/* Given langs: a, b, c:
+  Q1 - a
+  Q2 - b
+  Q3 - c
+  Q4 - if (a & b) || (a & c) = a
+  Q5 - if (a & b) || (b & c) = b
+  Q6 - if (a || b) && (c) = c
+*/
