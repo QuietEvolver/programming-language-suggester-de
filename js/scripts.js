ddd7b4f00102f8bbe4676c6471809fe3c43cd746
VW <34698193+QuietEvolver@users.noreply.github.com>
2022-10-21 13:07:11 -0700

    It will remove unnecessary logs from js.

--- js/scripts.js
+++ js/scripts.js
@@ -50,13 +50,10 @@ function languageSelector(e){
    // language totals calculations
    if( totalValue <= 6 ){ 
     console.log("eval tot", totalValue);
-    console.log(" totV output", output);
     document.getElementById("language-1").removeAttribute("class");
-    output = "Res = Go: Like watermelon";
    }
-    document.getElementById("output").innerText = output;
 
-    console.log(" outside output", output);
+    document.getElementById("output").innerText = output;
 }
 // Time permitting, to be added: handleSubmission(languageSelector)
 window.addEventListener("load", function() {
